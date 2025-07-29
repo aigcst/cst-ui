ba477ca58190709812405472f52a4bd79e34b89d
aigcst <aigcst@outlook.com>
2025-07-29 21:12:13 +0800

    feat: Button, theme

---
+++ src/cst_ui/basic/theme.py
@@ -0,0 +1,316 @@
+'''主题相关配置'''
+
+import flet as ft
+from pathlib import Path
+from enum import Enum, unique
+from typing import Optional, Union, TypedDict, Any, List, Dict, Tuple, Sequence, Callable
+from dataclasses import dataclass
+
+# --- 2、字体 ---
+FONT_DIR = Path(list(Path(__file__).parents)[1], 'data/fonts').resolve()
+
+
+class Font:
+    name: str
+    path: str
+
+    def __init__(self, path: str):
+        self.name = Path(path).stem
+        self.path = path
+
+    def __repr__(self):
+        return f'{self.name}, {self.path}'
+
+
+fonts = [
+    Font(str(Path(FONT_DIR, "AlibabaPuHuiTi-3-55-Regular.otf"))),
+    Font(str(Path(FONT_DIR, "Consolas.ttf"))),
+]
+
+FONTS = dict(zip([font.name for font in fonts], [font.path for font in fonts]))
+
+
+# 1、颜色
+@dataclass
+class FeedbackStyle:
+    icon: str
+    color: str
+    color_light: str
+    color_accent: str
+    # heavy: str
+    # heavy_max: str
+
+
+primary_color_style = FeedbackStyle(
+    icon=ft.Icons.MESSAGE, color=ft.Colors.BLUE, color_light=ft.Colors.BLUE_300, color_accent=ft.Colors.BLUE_800
+)
+success_color_style = FeedbackStyle(
+    icon=ft.Icons.DONE, color=ft.Colors.GREEN, color_light=ft.Colors.GREEN_300, color_accent=ft.Colors.GREEN_800
+)
+warning_color_style = FeedbackStyle(
+    icon=ft.Icons.WARNING_ROUNDED,
+    color=ft.Colors.AMBER,
+    color_light=ft.Colors.AMBER_300,
+    color_accent=ft.Colors.AMBER_800,
+)
+error_color_style = FeedbackStyle(
+    icon=ft.Icons.CLOSE, color=ft.Colors.RED, color_light=ft.Colors.RED_300, color_accent=ft.Colors.RED_800
+)
+info_color_style = FeedbackStyle(
+    icon=ft.Icons.CLOSE, color=ft.Colors.GREY, color_light=ft.Colors.GREY_300, color_accent=ft.Colors.GREY_800
+)
+
+
+@unique
+class StyleType(Enum):
+    DEFAULT = 0  # 默认灰色
+    PRIMARY = 1  # 蓝色，#007bff
+
+    INFO = 5  # 浅蓝色：#17a2b8
+    SUCCESS = 2  # 绿色：#28a745
+    WARNING = 3  # 黄色：#ffc107
+    ERROR = 4  # 红色：#dc3545
+
+
+# 2、尺寸大小
+# small_switch = create_custom_switch(0.5, "小号开关")
+# normal_switch = create_custom_switch(0.8, "正常大小开关")
+# large_switch = create_custom_switch(1.1, "大号开关")
+# extra_large_switch = create_custom_switch(1.4, "特大号开关")
+@dataclass
+class BorderRadius:
+    # https://fluent2.microsoft.design/shapes
+    # Navigation bars, tab bars
+    none: int = 0
+    # Small badges
+    small: int = 2
+    # Buttons, dropdown
+    medium: int = 4
+    # Large buttons
+    large: int = 8
+    # Button sheets, popovers
+    x_large: int = 12
+
+
+class StrokeThickness(Enum):
+    # https://fluent2.microsoft.design/shapes
+    # web
+    thin = 1
+    thick = 2
+    thicker = 3
+    thickest = 4
+
+    thin_mobile = 1
+    thick_mobile = 2
+    thicker_mobile = 4
+    thickest_mobile = 6
+
+
+# --- 列间距 ---
+@dataclass
+class Spacing:
+    same_control: int = 8
+    different_control: int = 12
+    none = 0
+    XXS = 2
+    XS = 4
+    SNudge = 6
+    S = 8
+    MNudge = 10
+    M = 12
+    L = 16
+    XL = 20
+    XXL = 24
+    XXXL = 32
+
+
+class LayoutType(Enum):
+    # 表单的水平、竖直排布
+    HORIZONTAL = 'horizontal'
+    VERTICAL = 'vertical'
+
+    # xs
+    # s
+    # m
+    # l
+
+
+class SizeType(Enum):
+    small = "small"
+    medium = "medium"
+    large = "large"
+    x_large = "x_large"
+    xx_large = "xx_large"
+    xxx_large = "xxx_large"
+
+
+@dataclass
+class Color:
+    """基于 UI 稿"""
+
+    background = ft.Colors.GREY_400
+    primary = primary_color_style.color
+    info = info_color_style.color
+    success = success_color_style.color
+    warning = warning_color_style.color
+    error = error_color_style.color
+
+
+class Theme:
+    def __init__(self) -> None:
+        # 1、颜色：色彩规范
+        self.color: Color = Color()
+        self.bgcolor: str = "#2e2f3e"
+
+        # 2、尺寸：大小规范
+        self.font_size = 14
+        self.light_theme_text_color = ft.Colors.BLACK
+        self.dark_theme_text_color = ft.Colors.WHITE
+        # self.color: Color = dataclasses.field(default=Color)
+        self.text_default_color: str = ft.Colors.BLACK
+        self.text_active_color: str = "#353535"
+        self.text_disabled_color: str = "#666"
+
+        self.text_size = 16
+        self.mask_background_color: str = "#666"
+
+        self.border_radius = BorderRadius()
+        # self.border=
+        self.border_width = 1
+        self.border_color = "#666"
+        self.border_active_color = "#666"
+        self.card_border_color = "#666"
+
+        self.form_content_height = 40
+        # 3、字体
+
+        # 4、圆角
+
+        # 5、阴影
+        self.shadow = ft.BoxShadow(
+            spread_radius=0.2,
+            blur_radius=10,
+            color=ft.Colors.GREY_300,
+            offset=ft.Offset(0, 2.5),
+            blur_style=ft.ShadowBlurStyle.SOLID,
+        )
+
+        # 9、应用主题
+        self.ft_theme = ft.Theme(
+            # color_scheme_seed=self.color.primary,  # 用于算法推导主题颜色余下部分的种子颜色
+            # color_scheme=ft.ColorScheme(    # 自定义从color_scheme_seed派生的material颜色方案
+            # ),
+            text_theme=ft.TextTheme(),  # 与卡片和画布颜色形成对比的文本样式
+            primary_text_theme=ft.TextTheme(),  # 与主色调形成对比的文本主题
+            scrollbar_theme=ft.ScrollbarTheme(
+                thickness=4,
+                radius=10,
+                main_axis_margin=5,
+                cross_axis_margin=-10,
+                track_visibility=False,
+                thumb_visibility=False,
+                track_color={
+                    ft.ControlState.DEFAULT: ft.Colors.TRANSPARENT,
+                },
+                thumb_color={
+                    ft.ControlState.HOVERED: ft.Colors.TRANSPARENT,
+                    ft.ControlState.DEFAULT: ft.Colors.TRANSPARENT,
+                },
+            ),
+            tabs_theme=ft.TabsTheme(),
+            font_family="AlibabaPuHuiTi-3-55-Regular",  # 所有UI元素的基准字体
+            use_material3=True,  # use material 2: this setting is mainly for the app-bar's elevation
+            # visual_density=ft.ThemeVisualDensity.ADAPTIVEPLATFORMDENSITY,
+            page_transitions=ft.PageTransitionsTheme(  # Removing animation on route change.
+                android=ft.PageTransitionTheme.NONE,
+                ios=ft.PageTransitionTheme.NONE,
+                macos=ft.PageTransitionTheme.NONE,
+                windows=ft.PageTransitionTheme.NONE,
+                linux=ft.PageTransitionTheme.NONE,
+            ),
+            # system_overlay_style=ft.SystemOverlayStyle(),
+            # appbar_theme=ft.AppBarTheme(),
+            # badge_theme=ft.BadgeTheme(),
+            # banner_theme=ft.BannerTheme(),
+            # bottom_appbar_theme=ft.BottomAppBarTheme(),
+            # bottom_navigation_bar_theme=ft.BottomNavigationBarTheme(),
+            # bottom_sheet_theme=ft.BottomSheetTheme(),
+            # card_theme=ft.CardTheme(),
+            # checkbox_theme=ft.CheckboxTheme(),
+            # chip_theme=ft.ChipTheme(),
+            # date_picker_theme=ft.DatePickerTheme(),
+            # dialog_theme=ft.DialogTheme(),
+            # divider_theme=ft.DividerTheme(),
+            # expansion_tile_theme=ft.ExpansionTileTheme(),
+            # list_tile_theme=ft.ListTileTheme(),
+            # navigation_bar_theme=ft.NavigationBarTheme(),
+            # navigation_drawer_theme=ft.NavigationDrawerTheme(),
+            # navigation_rail_theme=ft.NavigationRailTheme,
+            # popup_menu_theme=ft.PopupMenuTheme(),
+            # primary_color=self.color.primary,
+            # primary_color_dark=self.color.text_primary,
+            # primary_color_light=self.color.primary,
+            # primary_swatch=ft.Colors.RED,
+            # progress_indicator_theme=ft.ProgressIndicatorTheme(),
+            radio_theme=ft.RadioTheme(fill_color=ft.Colors.RED),
+            # search_bar_theme=ft.SearchBarTheme(),
+            # segmented_button_theme=ft.SegmentedButtonTheme(),
+            # slider_theme=ft.SliderTheme(),
+            # snackbar_theme=ft.SnackBarTheme(),
+            # switch_theme=ft.SwitchTheme(),
+            # time_picker_theme=ft.TimePickerTheme(),
+            # tooltip_theme=ft.TooltipTheme(),
+        )
+
+    def __getitem__(self, key: StyleType) -> FeedbackStyle:
+        if key == StyleType.INFO:
+            return info_color_style
+        elif key == StyleType.SUCCESS:
+            return success_color_style
+        elif key == StyleType.WARNING:
+            return warning_color_style
+        elif key == StyleType.ERROR:
+            return error_color_style
+        elif key == StyleType.DEFAULT:
+            return primary_color_style
+        elif key == StyleType.PRIMARY:
+            return primary_color_style
+        else:
+            return primary_color_style
+
+    def set_theme(self, page: ft.Page, on_resize: Callable):
+        # 设置字体
+        page.fonts = FONTS
+        # 设置主题
+        page.theme_mode = ft.ThemeMode.LIGHT
+        page.theme = theme.ft_theme
+        page.bgcolor = Color.background
+        # page.vertical_alignment = ft.MainAxisAlignment.CENTER
+        # page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+        page.scroll = ft.ScrollMode.AUTO
+        page.spacing = 0
+        page.padding = 0
+        page.scroll = ft.ScrollMode.AUTO
+        page.window.resizable = True  # 页面缩放
+        # page.on_resize = lambda _: on_page_resize(_, on_resize)
+        # page.window.full_screen = True
+        # page.window_maximizable = False
+        # page.window_always_on_top = True
+        # 最大化
+        page.window.maximizable = True
+        page.window.maximized = True
+        # page.window.center()
+        # page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+        # page.vertical_alignment = ft.MainAxisAlignment.START
+        page.window.width = 800
+        page.window.height = 300
+
+    # def switch(self, theme_mode: ft.ThemeMode):
+    #     self.page.client_storage.set("theme_mode", self.mode)
+    #     self.page.theme_mode = self.mode[0]
+    #     self.label = self.mode[1]
+    #     self.page.update()
+
+
+theme = Theme()
---
+++ src/cst_ui/form/button.py
@@ -0,0 +1,220 @@
+'''按钮'''
+
+import flet as ft
+from enum import Enum
+from typing import Optional, Unpack
+from cst_ui.basic.theme import theme, StyleType
+
+
+class ButtonSizeType(Enum):
+    small = 30
+    medium = 36
+    large = 42
+
+
+class ButtonShapeType(Enum):
+    # 直角
+    rect = 'rect'
+    # 圆弧（默认）
+    circle = 'circle'
+    # 半圆
+    round = 'round'
+
+
+class Button(ft.Button):
+
+    def __init__(
+        self,
+        text: Optional[str] = None,
+        style_type: StyleType = StyleType.DEFAULT,
+        size_type: ButtonSizeType = ButtonSizeType.medium,
+        shape_type: ButtonShapeType = ButtonShapeType.round,
+        plain=False,  # 中心是否镂空
+        **kwargs,
+    ):
+        # kwargs.setdefault('icon_color', ft.Colors.WHITE)
+        super().__init__(**kwargs)
+        self.text = text
+        self.update_content()
+
+        self.style_type = style_type
+        self.shape_type = shape_type
+        self.size_type = size_type
+        self.plain = plain
+        self.height = size_type.value
+
+        self.height = size_type.value
+        # self.content = ft.Text(value=text)
+        self.style = ft.ButtonStyle(
+            color={
+                ft.ControlState.DEFAULT: ft.Colors.WHITE,
+            },
+            bgcolor={
+                ft.ControlState.DEFAULT: theme[self.style_type].color,
+                ft.ControlState.HOVERED: theme[self.style_type].color_light,
+                ft.ControlState.DISABLED: ft.Colors.GREY_300,
+                ft.ControlState.PRESSED: theme[self.style_type].color_accent,
+            },
+            # 点击样式
+            overlay_color={
+                ft.ControlState.DEFAULT: theme[self.style_type].color,
+                ft.ControlState.HOVERED: theme[self.style_type].color_light,
+                ft.ControlState.DISABLED: ft.Colors.GREY_300,
+                ft.ControlState.PRESSED: theme[self.style_type].color_accent,
+                # ft.ControlState.PRESSED: theme[self.style_type].color_light,
+                # ft.ControlState.PRESSED: ft.Colors.RED,
+            },
+            shadow_color={
+                # jui.theme[self.style_type].color_side_color,
+                ft.ControlState.DEFAULT: theme[self.style_type].color,
+                ft.ControlState.HOVERED: theme[self.style_type].color_light,
+                ft.ControlState.DISABLED: ft.Colors.GREY_300,
+                ft.ControlState.PRESSED: theme[self.style_type].color_accent,
+                # ft.ControlState.PRESSED: theme[self.style_type].color_light,
+                # ft.ControlState.PRESSED: ft.Colors.RED,
+            },
+            surface_tint_color=None,
+            elevation={
+                ft.ControlState.DEFAULT: 0,
+                ft.ControlState.PRESSED: 0,
+                ft.ControlState.HOVERED: 0,
+            },
+            # animation_duration=500,
+            # padding=ft.padding.only(left=2, right=2),
+            side={
+                ft.ControlState.DEFAULT: ft.BorderSide(1, theme[self.style_type].color),
+                ft.ControlState.HOVERED: ft.BorderSide(1, theme[self.style_type].color_light),
+                ft.ControlState.DISABLED: ft.BorderSide(1, ft.Colors.GREY),
+                ft.ControlState.PRESSED: ft.BorderSide(1, theme[self.style_type].color_accent),
+            },
+        )
+        if self.plain:
+            self.style.bgcolor = {
+                ft.ControlState.DEFAULT: ft.Colors.WHITE,
+                # ft.ControlState.PRESSED: jui.theme[self.style_type].color_light_max,
+            }
+            # 点击样式
+            self.style.overlay_color = {
+                ft.ControlState.DEFAULT: ft.Colors.WHITE,
+                # ft.ControlState.PRESSED: jui.theme[self.style_type].color_light_max,
+                # ft.ControlState.PRESSED: theme[self.style_type].color_light,
+                # ft.ControlState.PRESSED: ft.Colors.RED,
+            }
+            self.style.side = {
+                ft.ControlState.DEFAULT: ft.BorderSide(1, ft.Colors.GREY),
+                ft.ControlState.HOVERED: ft.BorderSide(1, theme[self.style_type].color_light),
+                ft.ControlState.DISABLED: ft.BorderSide(1, ft.Colors.GREY),
+                ft.ControlState.PRESSED: ft.BorderSide(2, theme[self.style_type].color_accent),
+            }
+            self.style.color = {
+                ft.ControlState.DEFAULT: ft.Colors.BLACK,
+                ft.ControlState.HOVERED: theme[self.style_type].color,
+                ft.ControlState.PRESSED: ft.Colors.WHITE,
+            }
+
+        if self.shape_type == ButtonShapeType.rect:
+            self.style.shape = ft.ContinuousRectangleBorder()
+        elif self.shape_type == ButtonShapeType.round:
+            self.style.shape = ft.RoundedRectangleBorder(radius=theme.border_radius.large)
+        elif self.shape_type == ButtonShapeType.circle:
+            self.style.shape = ft.StadiumBorder()
+
+        if self.disabled:
+            self.style.bgcolor = ft.Colors.GREY_300
+
+    def update_content(self):
+        # 组件逻辑：更新时获取 content
+        if self.content is None and self.text is not None:
+            self.content = ft.Text(self.text)
+
+    def set_disabled(self, disabled: bool):
+        self.disabled = disabled
+        if disabled:
+            pass
+            # self.bgcolor = ButtonColors.button_disabled_background_color
+            # self.color = ButtonColors.button_disabled_content_color
+            # self.on_click = None
+        else:
+            pass
+            # self.bgcolor = ButtonColors.button_backgound_color
+            # self.color = ButtonColors.button_content_color
+            # self.on_click = self.on_click
+        self.update_content()
+
+
+def main(page: ft.Page):
+    def click_button(e):
+        print('Button: Test: clicked button')
+
+    page.window.width = 370
+    page.window.always_on_top = True
+
+    page.add(
+        Button(content=ft.Text('test')),
+        ft.Text('基本使用'),
+        ft.Text('示例'),
+        ft.Text('源码'),
+        ft.Text('注意事项'),
+        ft.Text('主按钮'),
+        ft.Row(
+            controls=[
+                Button(text='color_button', style_type=StyleType.DEFAULT),
+                Button(text='primary_button', style_type=StyleType.PRIMARY),
+                Button(text='success_button', style_type=StyleType.SUCCESS),
+                Button(text='info_button', style_type=StyleType.INFO),
+                Button(text='warning_button', style_type=StyleType.WARNING),
+                Button(text='error_button', style_type=StyleType.ERROR),
+            ]
+        ),
+        ft.Text('次按钮，描边按钮，plain'),
+        ft.Row(
+            controls=[
+                Button(text='color_button', style_type=StyleType.DEFAULT, plain=True),
+                Button(text='primary_button', style_type=StyleType.PRIMARY, plain=True),
+                Button(text='success_button', style_type=StyleType.SUCCESS, plain=True),
+                Button(text='info_button', style_type=StyleType.INFO, plain=True),
+                Button(text='warning_button', style_type=StyleType.WARNING, plain=True),
+                Button(text='error_button', style_type=StyleType.ERROR, plain=True),
+            ]
+        ),
+        ft.Text('rect-round-circle'),
+        ft.Row(
+            controls=[
+                Button(
+                    text='ButtonShapeType.rect',
+                    style_type=StyleType.DEFAULT,
+                    plain=True,
+                    shape_type=ButtonShapeType.rect,
+                ),
+                Button(
+                    text='ButtonShapeType.round',
+                    style_type=StyleType.DEFAULT,
+                    plain=True,
+                    shape_type=ButtonShapeType.round,
+                ),
+                Button(
+                    text='ButtonShapeType.circle',
+                    style_type=StyleType.DEFAULT,
+                    plain=True,
+                    shape_type=ButtonShapeType.circle,
+                ),
+            ]
+        ),
+        ft.Text('disable'),
+        ft.Row(
+            controls=[
+                Button(text='color_button', style_type=StyleType.DEFAULT, plain=True, disabled=True),
+                Button(text='primary_button', style_type=StyleType.PRIMARY, plain=True, disabled=True),
+                Button(text='success_button', style_type=StyleType.SUCCESS, plain=True, disabled=True),
+                Button(text='info_button', style_type=StyleType.INFO, plain=True, disabled=True),
+                Button(text='warning_button', style_type=StyleType.WARNING, plain=True, disabled=True),
+                Button(text='error_button', style_type=StyleType.ERROR, plain=True, disabled=True),
+            ]
+        ),
+        ft.Text('icon_button'),
+        Button(text='icon_button', icon=ft.Icons.ADD),
+    )
+
+
+if __name__ == '__main__':
+    ft.run(main)
